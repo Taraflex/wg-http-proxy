dec892c16f946751076901e4e52823222ef92050
Taraflex <taraflex.red@gmail.com>
2022-08-30 05:33:23 +0300

    1

--- .github/workflows/generate-pac.sh
+++ .github/workflows/generate-pac.sh
@@ -22,14 +22,8 @@ PACFILE_NOSSL_MIN="${PACFILE_NOSSL%.*}.min.js"
 npx terser $PACFILE_NOSSL --ecma 5 --toplevel --mangle reserved=[FindProxyForURL,url,host] -o $PACFILE_NOSSL_MIN
 sed -i "1s/^/\/\/ Generated on $(date --utc)\n/" $PACFILE_NOSSL_MIN
 
-sed -i 's+function FindProxyForURL(url, host) {+function FindProxyForURL(url, host) {  
-  {{#PACPROXYRE}} if (/{{{PACPROXYRE}}}/i.test(host)) return "PROXY {{{PACPROXYHOST}}}; DIRECT"; {{/PACPROXYRE}}
-  {{#PACDIRECTRE}} if (/{{{PACDIRECTRE}}}/i.test(host)) return "DIRECT"; {{/PACDIRECTRE}} 
-+' $PACFILE_NOSSL
-sed -i 's+function FindProxyForURL(url, host) {+function FindProxyForURL(url, host) {  
-  {{#PACPROXYRE}} if (/{{{PACPROXYRE}}}/i.test(host)) return "PROXY {{{PACPROXYHOST}}}; DIRECT"; {{/PACPROXYRE}}
-  {{#PACDIRECTRE}} if (/{{{PACDIRECTRE}}}/i.test(host)) return "DIRECT"; {{/PACDIRECTRE}} 
-+' $PACFILE_NOSSL_MIN
+sed -i 's+function FindProxyForURL(url, host) {+function FindProxyForURL(url, host) {\n  {{#PACPROXYRE}} if (/{{{PACPROXYRE}}}/i.test(host)) return "PROXY {{{PACPROXYHOST}}}; DIRECT"; {{/PACPROXYRE}}\n  {{#PACDIRECTRE}} if (/{{{PACDIRECTRE}}}/i.test(host)) return "DIRECT"; {{/PACDIRECTRE}}\n+' $PACFILE_NOSSL
+sed -i 's+function FindProxyForURL(url, host) {+function FindProxyForURL(url, host) {\n  {{#PACPROXYRE}} if (/{{{PACPROXYRE}}}/i.test(host)) return "PROXY {{{PACPROXYHOST}}}; DIRECT"; {{/PACPROXYRE}}\n  {{#PACDIRECTRE}} if (/{{{PACDIRECTRE}}}/i.test(host)) return "DIRECT"; {{/PACDIRECTRE}}\n+' $PACFILE_NOSSL_MIN
 
 brotli -kf $PACFILE_NOSSL_MIN -o $PACFILE_NOSSL_MIN.br
 brotli -kf $PACFILE_NOSSL -o $PACFILE_NOSSL.br
